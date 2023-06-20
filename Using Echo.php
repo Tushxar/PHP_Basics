0d4d1c7e2df7e11ff90f5a5712690a5588041da7
Tushar <136239063+Tushxar@users.noreply.github.com>
2023-06-20 15:33:04 +0530

    Create Using Echo.php

---
+++ Using Echo.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+
+
+$jake = " Hello Everyone";
+ECHO "Hello World!<br>";
+echo "Hello World!<br>";
+EcHo "Hello World!<br>";
+echo " hi myself Tushar $jake! <br>";
+
+
+echo " hi everyone .$jake <br> ";
+  
+  // example that echo is not a case sensative 
+  // used for displaying lines of text or string which are passed as arguments on the command line
+
+
+?> 
+
+</body>
+</html>
